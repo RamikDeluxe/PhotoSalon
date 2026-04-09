6a1ad7b43bed8c575c7cbb80fb1b9e35f3617cc7
Ramik <33555282+RamikDeluxe@users.noreply.github.com>
2026-04-09 11:35:00 +0300

    Update photosalonramik.sql
    Файл для создания базы данных

--- photosalonramik.sql
+++ photosalonramik.sql
@@ -17,11 +17,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- База данных: `photosalonramik`
---
+CREATE DATABASE IF NOT EXISTS `photosalonramik`
+DEFAULT CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
 
--- --------------------------------------------------------
+USE `photosalonramik`;
 
 --
 -- Структура таблицы `feedback`
